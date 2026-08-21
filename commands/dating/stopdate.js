d594876b8a1773f922259a040f96c1d1f9e6b3c0
VK911 TECH <gbolahanraifu1@gmail.com>
2026-08-21 03:10:53 +0100

    Add dating command stopdate

---
+++ commands/dating/stopdate.js
@@ -0,0 +1,1 @@
+const d=require('../../lib/dating');module.exports={name:'stopdate',aliases:['pausedate'],category:'dating',desc:'Pause matchmaking',usage:'†stopdate',privateOnly:true,async execute(s,m,a,c){return d.toggle(c,false);}};
