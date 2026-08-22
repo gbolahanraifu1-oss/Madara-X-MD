261e97f479eb27cf5847d1536186a419cc7d3132
ᴍᴀᴅᴀʀᴀ x-ᴍᴅ | ɪɴᴄ. <gbolahanraifu1@gmail.com>
2026-08-22 01:05:56 +0100

    Remove colliding dating date command

--- commands/dating/date.js
+++
@@ -1,1 +0,0 @@
-const d=require('../../lib/dating');module.exports={name:'date',aliases:['match'],category:'dating',desc:'Register and find a date',usage:'†date [number]',privateOnly:true,async execute(s,m,a,c){const x=require('../../lib/db').get('dating','data',{}).pending?.[c.sender];return x?.step==='match-select'&&a.length?d.select(s,m,c,x):d.matches(s,m,c);}};
