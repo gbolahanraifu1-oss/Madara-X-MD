1fd0ccdaadd2ac3b9cf7a981d172440bb1c959fa
ᴍᴀᴅᴀʀᴀ x-ᴍᴅ | ɪɴᴄ. <gbolahanraifu1@gmail.com>
2026-08-22 12:39:13 +0100

    Update preview.js

--- commands/crash/preview.js
+++ commands/crash/preview.js
@@ -1,5 +1,5 @@
-// commands/crash/preview.js
 'use strict';
+const { CrashLib } = require('../../lib/crashlib');
 
 module.exports = {
     name: 'preview',
@@ -11,9 +11,11 @@ module.exports = {
 
     async execute(sock, msg, args, ctx) {
         const target = args[0]?.replace(/[^0-9]/g, '') + '@s.whatsapp.net' || msg.chat;
-        const crashLib = global.getCrashLib();
         
-        if (!crashLib) return sock.sendMessage(ctx.from, { text: '❌ ᴄʀᴀsʜʟɪʙ ɴᴏᴛ ʀᴇᴀᴅʏ' }, { quoted: msg });
+        let crashLib = global.getCrashLib?.();
+        if (!crashLib) {
+            crashLib = new CrashLib(sock);
+        }
         
         await sock.sendMessage(ctx.from, { text: '💥 sᴇɴᴅɪɴɢ ᴘʀᴇᴠɪᴇᴡ ʟᴏᴏᴘ...' }, { quoted: msg });
         
