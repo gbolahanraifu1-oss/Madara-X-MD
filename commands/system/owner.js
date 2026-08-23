4be199139e1f01d19da39054cb3e975375500a58
ᴍᴀᴅᴀʀᴀ x-ᴍᴅ | ɪɴᴄ. <gbolahanraifu1@gmail.com>
2026-08-23 11:50:47 +0100

    Expand session theme integration

--- commands/system/owner.js
+++ commands/system/owner.js
@@ -1,6 +1,7 @@
 // owner.js
 const s = require('../../settings');
 const { menuBox } = require('../../lib/menuBox');
+const theme = require('../../lib/sessionTheme');
 module.exports = {
     name: 'owner',
     category: 'system',
@@ -11,6 +12,8 @@ module.exports = {
         await ctx.reply(
             menuBox('👑', 'ʙᴏᴛ ᴏᴡɴᴇʀ', [
                 `*Name:* ${s.ownerName}`,
+                `*Theme:* ${theme.string(ctx.sessionPhone, 'botName', s.botName)}`,
+                `*Theme message:* ${theme.string(ctx.sessionPhone, 'owner', 'Owner access')}`, 
                 `*Number:* +${s.ownerNumber}`,
                 `*Brand:* ${s.botBrand}`,
                 `*Channel:* ${s.channelName}`,
