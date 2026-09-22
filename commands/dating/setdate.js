5562991b7c18ff62311d86385be383d85851b5ec
ᴍᴀᴅᴀʀᴀ x-ᴍᴅ | ɪɴᴄ. <gbolahanraifu1@gmail.com>
2026-09-22 14:54:28 +0100

    Delete setdate.js

--- commands/dating/setdate.js
+++
@@ -1,1 +0,0 @@
-const d=require('../../lib/dating');module.exports={name:'setdate',category:'dating',desc:'Edit your dating profile',usage:'†setdate bio I love music',privateOnly:true,async execute(s,m,a,c){return d.setDate(c,a);}};
